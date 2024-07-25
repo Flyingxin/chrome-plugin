d67077f9432f8c0c8f1c90b66cb3cd992375fcbb
ccyx <702609592@qq.com>
2024-07-26 01:00:39 +0800

    #FEAT: popup页交互功能

--- content.js
+++ content.js
@@ -1,75 +1,75 @@
 console.warn("content.js Welcome");
 
+let timer = null;
 // 接收消息
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.from === "background") {
-    console.log("background -> content", request.data);
-    // 回消息给background
-    sendResponse("收到了你的情书💌");
-  }
   if (request.from === "popup") {
-    console.log("popup -> content", request.data);
-    // 回消息给popup
-    sendResponse("收到了你的情书💌");
+    switch (request.data) {
+      case "getOrderInfo":
+        getMyOrders();
+        break;
+      case "intervalGetOrderInfo":
+        if (timer) clearInterval(timer);
+        timer = setInterval(() => {
+          getMyOrders();
+        }, 3000);
+        break;
+      case "closeOrderInfo":
+        clearInterval(timer);
+        timer = null;
+        break;
+    }
   }
 });
 
-// 发送消息
-chrome.runtime.sendMessage(
-  // 发送消息给 background
-  { from: "content", data: "Hello!" },
-  // 接收 background 回复
-  (response) => {
-    console.log("content 接收 background", response);
+const getMyOrders = async () => {
+  let response = await fetch(
+    "https://h5api.m.taobao.com/h5/mtop.alibaba.topservice.order.list.get/1.0/?jsv=2.6.1&appKey=12574478&t=1721924912070&sign=1ebd1eef54e1667262e1327327537e16&api=mtop.alibaba.topservice.order.list.get&v=1.0&valueType=string&dataType=originaljsonp&type=jsonp&callback=mtopjsonp8&data=%7B%22env%22%3A%22online%22%2C%22payUrl%22%3A%22%2Fser%2ForderPayNew.htm%22%2C%22pageSize%22%3A10%2C%22tabCode%22%3A%22all%22%2C%22currentPage%22%3A%221%22%7D",
+    {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        cookie:
+          "cookie2=15ee3d4a7e6f7655974c14437ebd3fcd; t=3e301986d4d5cdce6ad4f38ea4bcd1b9; _tb_token_=786347eb8e5e3; xlly_s=1; mtop_partitioned_detect=1; _m_h5_tk=d2c96cb787a49cc882c054943eec743a_1721930820600; _m_h5_tk_enc=6eb78f2686ced84366b8a87cc0b07633; 3PcFlag=1721923626893; unb=2201478220047; lgc=tb616717184; cancelledSubSites=empty; cookie17=UUphy%2FZ9uP8pZ4EReg%3D%3D; dnk=tb616717184; tracknick=tb616717184; _l_g_=Ug%3D%3D; sg=47d; _nk_=tb616717184; cookie1=VqgqprOsJjmNLKN9yLW%2BCWHBA77k2PIKji0Cz4OuRiw%3D; sgcookie=E100%2BvXHC3nFfi%2BkbTWjBr8MOASuaOEPCesOtR5f7QEork%2Fx7vWe3GZM%2Bf5Vc02NIM7zAid3FSik2F9IPGtEi3K1B%2FsaLKPi1i7UF8DdnEqO9y1jQipKn5OiSQsMJ7j%2FHuoi; havana_lgc2_0=eyJoaWQiOjIyMDE0NzgyMjAwNDcsInNnIjoiMmM2OTZmZjM3MzA2NTY0ZWY4ZGI2YThkMjg5ZDRjNWYiLCJzaXRlIjowLCJ0b2tlbiI6IjFZYmM4RjFJb2pVQVo3TmFzNUlpQzZnIn0; _hvn_lgc_=0; havana_lgc_exp=1753027632715; cookie3_bak=15ee3d4a7e6f7655974c14437ebd3fcd; cookie3_bak_exp=1722182832715; wk_cookie2=1b42098fcf381975d55a76822ba2ddce; wk_unb=UUphy%2FZ9uP8pZ4EReg%3D%3D; sn=; uc3=vt3=F8dD3ijGtwcNPo%2Bk6TU%3D&nk2=F5RDLeMsPO8CNfs%3D&id2=UUphy%2FZ9uP8pZ4EReg%3D%3D&lg2=WqG3DMC9VAQiUQ%3D%3D; csg=f7830298; env_bak=FM%2Bgz3ym37t%2FFkdzMQz5dhi%2BaoyJj6KzBxSLaUu3pN%2Fi; skt=55b1a40373fc9475; existShop=MTcyMTkyMzYzMg%3D%3D; uc4=id4=0%40U2grEJGCKQ3F8IwvzBE%2FosNrW91A03bc&nk4=0%40FY4I7jF0b%2FrkW2%2FZ28lZEXl2X3Y5Ug%3D%3D; _cc_=Vq8l%2BKCLiw%3D%3D; uc1=cookie14=UoYcAjgTl0l7LQ%3D%3D; tfstk=fkpj6gZAGr4XTF-5SsnPdBzp0IX1lEMUCls9xhe4XtBY1RKpA1eZit51CUQy3Z-VHoAk8He23jXaCtXGB2uELv-2mOXT8YuHarXRAG2tQZIOyIFOF2uELYlY2Ogj8ClQ0iC5faIAXZCxVzIGAOIAD1n5yGs36rLOB0tRrMSYWieO2zIGbv9oFgoNlni5ubOW5jr2oawTdujfMUHhyRe9Fit5JnF7BRp5cs9aGiUg8sR9x6v2HAwl3HOReM-mh86pDMTVzeHLFTKy2F5HZV2FZU916KC0X0QJhU9cHElTde6X56pANlewAnQfFgpKbWbyFZ89h_nUd1WJL6Bvab0V_tsBWKYbfRLp4HJcZdgQyNAVxTIemxw5pnIO4LyFRYh_1u13Cg_EV0Ng_GfiIzN82ltPMgjz80i7lTCAqg_EV0NgssIl4DoSVr6R.; isg=BEREPMSp_pWngkraG_ZWqwkFFcI2XWjHAa1CwF7lmo_SieVThmtaV15hzSlRkaAf",
+        Referer: "https://fuwu.taobao.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+      body: null,
+      method: "GET",
+      credentials: "include",
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
-);
-// 不能给popup发消息，是因为popup没有打开，所以popup没有监听消息的机会
-
-// const getMyOrders = async () => {
-//     let response = await fetch("https://h5api.m.taobao.com/h5/mtop.alibaba.topservice.order.list.get/1.0/?jsv=2.6.1&appKey=12574478&t=1721834347344&sign=f76e878b38d7c997e7376dc10181e9bf&api=mtop.alibaba.topservice.order.list.get&v=1.0&valueType=string&dataType=originaljsonp&type=jsonp&callback=mtopjsonp7&data=%7B%22env%22%3A%22online%22%2C%22payUrl%22%3A%22%2Fser%2ForderPayNew.htm%22%2C%22pageSize%22%3A10%2C%22tabCode%22%3A%22all%22%2C%22currentPage%22%3A%221%22%7D", {
-//         "headers": {
-//           "accept": "*/*",
-//           "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-//           "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
-//           "sec-ch-ua-mobile": "?0",
-//           "sec-fetch-dest": "script",
-//           "sec-fetch-mode": "no-cors",
-//           "sec-fetch-site": "same-site"
-//         },
-//         "referrer": "https://fuwu.taobao.com/",
-//         "referrerPolicy": "strict-origin-when-cross-origin",
-//         "body": null,
-//         "method": "GET",
-//         "mode": "cors",
-//         "credentials": "include"
-//       });
-//     if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     response = await response.text();
-//     response = parseJSONP(response);
-//     console.log('response', response.data.list);
-//     const orderNameList = response.data.list.map(item => item.articleName);
-//     chrome.runtime.sendMessage(chrome.runtime.id, {
-//         from: "myOrders",
-//         data: orderNameList,
-//     })
-// }
-// const inTimer = setInterval(() => {
-//     getMyOrders()
-// }, 2000);
-// getMyOrders()
+  response = await response.text();
+  response = parseJSONP(response);
+  if (!response.data.list) {
+    console.error(response.ret[0]);
+    return;
+  }
+  const orderNameList = response.data.list.map((item) => item.articleName);
+  console.log("orderNameList", orderNameList);
+};
 
 // 解析 JSONP 格式的字符串
-// const parseJSONP = (jsonpString) => {
-//     // 假设 JSONP 格式为 mtopjsonp7({...})
-//     const jsonpPattern = / *mtopjsonp[1-9]\((.*)\);?$/;
-//     const match = jsonpString.match(jsonpPattern);
-//     if (match) {
-//         // 提取 JSON 字符串
-//         const jsonString = match[1];
-//         // 解析 JSON 字符串为对象
-//         return JSON.parse(jsonString);
-//     }
-//     throw new Error('Invalid JSONP format');
-// };
+const parseJSONP = (jsonpString) => {
+  // 假设 JSONP 格式为 mtopjsonp7({...})
+  const jsonpPattern = / *mtopjsonp[1-9]\((.*)\);?$/;
+  const match = jsonpString.match(jsonpPattern);
+  if (match) {
+    // 提取 JSON 字符串
+    const jsonString = match[1];
+    // 解析 JSON 字符串为对象
+    return JSON.parse(jsonString);
+  }
+  throw new Error("Invalid JSONP format");
+};
--- popup.js
+++ popup.js
@@ -1,38 +1,41 @@
 console.warn("popup.js Welcome");
-const button = document.getElementById("go");
-button.addEventListener("click", async function () {
-  // 发送消息给background
-  chrome.runtime.sendMessage(
-    // 发送消息给 background
-    { from: "popup", data: "Hello!" },
-    // 接收 background 回复
-    (response) => {
-      console.log("popup 收到 background", response);
-    }
-  );
+const go = document.getElementById("go");
+const interval = document.getElementById("interval");
+const leave = document.getElementById("leave");
+
+// 获取订单信息
+go.addEventListener("click", async function () {
   // 发送消息给content
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(
       // tabId
       tabs[0].id,
       // 发送消息给 content
-      { from: "popup", data: "Hello!" },
-      // 接收 content 回复
-      (response) => {
-        console.log("popup 收到 content", response);
-      }
+      { from: "popup", data: "getOrderInfo" }
     );
   });
 });
-
-// 无法直接获取接收消息，popup没有打开，所以popup没有加载，所以popup没有监听消息的机会
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.from === "background") {
-//     console.log("popup 接收 background", request.data);
-//   }
-//   if (request.from === "content") {
-//     console.log("popup 接收 content", request.data);
-//   }
-// });
-// 按道理还是可以接收得到，但是popup没有打开的需求没有意义
-// 插件执行过程中接收按理说可以，拭目以待
+// 开启定时获取订单信息
+interval.addEventListener("click", async function () {
+  // 发送消息给content
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      // tabId
+      tabs[0].id,
+      // 发送消息给 content
+      { from: "popup", data: "intervalGetOrderInfo" }
+    );
+  });
+});
+// 关闭订单信息
+leave.addEventListener("click", async function () {
+  // 发送消息给content
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      // tabId
+      tabs[0].id,
+      // 发送消息给 content
+      { from: "popup", data: "closeOrderInfo" }
+    );
+  });
+});
